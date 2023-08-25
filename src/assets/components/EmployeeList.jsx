8afc31554d9ae79f94af85e0076be143f9ab2bc2
o achamfo <owusuachamfour@gmail.com>
2023-08-25 05:40:41 -0400

    Homepage comeplete.

--- src/assets/components/EmployeeList.jsx
+++ src/assets/components/EmployeeList.jsx
@@ -3,7 +3,12 @@ import EmployeeListItem from "./EmployeeListItem";
 function EmployeeList() {
   return (
     <div className="EmployeeList">
-      <EmployeeListItem employeeName="First Last" employeeTitle="CEO" />
+      <EmployeeListItem employeeName="First0 Last0" employeeTitle="PRESIDENT" />
+      <EmployeeListItem
+        employeeName="First1 Last1"
+        employeeTitle="VICE PRESIDENT"
+      />
+      <EmployeeListItem employeeName="First2 Last2" employeeTitle="CEO" />
     </div>
   );
 }
