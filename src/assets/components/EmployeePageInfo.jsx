31fdedb92266794a3a2e8165c3cb1842eb54f702
“Owusu <owusuachamfour@gmail.com>
2023-08-27 14:03:27 -0400

    img

--- src/assets/components/EmployeePageInfo.jsx
+++ src/assets/components/EmployeePageInfo.jsx
@@ -5,7 +5,7 @@ function EmployeePageInfo(props) {
         <div className="EmployeePageInfoPicDiv">
           <img
             className="EmployeePageInfoPic"
-            src="../test/car.jfif"
+            src="/test/car.jfif"
             alt="Employee Page Info Pic"
           />
         </div>
