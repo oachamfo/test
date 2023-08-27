8c3a3ca72cad6e8a4b02d4f9186e5fdff38a4444
“Owusu <owusuachamfour@gmail.com>
2023-08-27 08:39:59 -0400

    experimenting with images

--- src/assets/components/EmployeePageInfo.jsx
+++ src/assets/components/EmployeePageInfo.jsx
@@ -5,7 +5,7 @@ function EmployeePageInfo(props) {
         <div className="EmployeePageInfoPicDiv">
           <img
             className="EmployeePageInfoPic"
-            src="/src/assets/components/images/owusu_pic.jfif"
+            src="https://oachamfo.github.io/test/src/assets/images/owusu_pic.jfif"
             alt="Employee Page Info Pic"
           />
         </div>
