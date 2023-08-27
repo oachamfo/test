5846e668e08f57991dec17eb0df80c91adf3196b
“Owusu <owusuachamfour@gmail.com>
2023-08-27 08:17:05 -0400

    fixing img link

--- src/assets/components/EmployeePageInfo.jsx
+++ src/assets/components/EmployeePageInfo.jsx
@@ -5,7 +5,7 @@ function EmployeePageInfo(props) {
         <div className="EmployeePageInfoPicDiv">
           <img
             className="EmployeePageInfoPic"
-            src="/src/components/images/owusu_pic.jfif"
+            src="/src/assets/components/images/owusu_pic.jfif"
             alt="Employee Page Info Pic"
           />
         </div>
